d2790ea81ffd5f91b6647dbbbf6484ae9ae7a82e
avinav2401 <avinavpriyadrshi@gmail.com>
2026-03-02 10:54:45 +0530

    ok

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,29 +1,30 @@
 /*
 This class will print the details of the App.
 * @author Avinav priyadrshi
-* @Version 1.0
+* @Version 9.0
 *
 * */
 import java.util.*;
 public class PalindromeCheckerApp {
+    private static boolean check(String s, int start, int end) {
+        if (start < end) {
+            if (s.charAt(start) != s.charAt(end))
+                return false;
+            else
+                return check(s, start + 1, end - 1);
+        }
+        else
+            return true;
+    }
+
+
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a word : ");
-        String wrd=sc.nextLine(),temp = "";
-        LinkedList<Character> list=new LinkedList<>();
-        for(char c: wrd.toCharArray())
-        {
-            list.add(c);
-        }
-        boolean isPalindrome=true;
-        while(list.size()>1)
-        {
-            if(list.removeFirst()!=list.removeLast())
-            {
-                isPalindrome=false;
-                break;
-            }
-        }
+       String wrd=sc.nextLine(),temp = "";
+
+        boolean isPalindrome=check(wrd,0,wrd.length()-1);
+
         if(isPalindrome)
             System.out.println("word is Palindrome");
         else
