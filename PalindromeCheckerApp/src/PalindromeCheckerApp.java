ab398960bd1c2d9790ee34ed5992f349db5b529c
avinav2401 <avinavpriyadrshi@gmail.com>
2026-03-02 11:04:54 +0530

    ok

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -11,12 +11,28 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        PalindromeStrategy strategy = new StackStrategy();
         System.out.println("Enter a word : ");
-       String wrd=sc.nextLine();
-        boolean isPalindrome = strategy.check(wrd);
-        System.out.println("Input : " + wrd);
-        System.out.println("Is Palindrome? : " + isPalindrome);
+        String wrd=sc.nextLine();
+        PalindromeStrategy stackStrategy = new StackStrategy();
+        PalindromeStrategy twoPointerStrategy = new TwoPointerStrategy();
+
+        System.out.println("\n--- UC13: Performance Comparison ---");
+
+        long startTimeStack = System.nanoTime();
+        boolean res1 = stackStrategy.check(wrd);
+        long endTimeStack = System.nanoTime();
+        long durationStack = endTimeStack - startTimeStack;
+
+        long startTimeTP = System.nanoTime();
+        boolean res2 = twoPointerStrategy.check(wrd);
+        long endTimeTP = System.nanoTime();
+        long durationTP = endTimeTP - startTimeTP;
+
+        System.out.println("1. Stack Strategy       | Result: " + res1 + " | Time: " + durationStack + " ns");
+        System.out.println("2. Two-Pointer Strategy | Result: " + res2 + " | Time: " + durationTP + " ns");
+
+        System.out.println("\nPerformance Gap: " + (durationStack - durationTP) + " ns");
+
 
     }
 }
@@ -25,18 +41,23 @@ interface PalindromeStrategy {
 }
 
 class StackStrategy implements PalindromeStrategy {
-    @Override
     public boolean check(String input) {
-        if (input == null) return false;
-        String cleanedInput = input.toLowerCase();
+        String str = input.toLowerCase();
         Stack<Character> stack = new Stack<>();
-        for (char c : cleanedInput.toCharArray()) {
-            stack.push(c);
+        for (char c : str.toCharArray()) stack.push(c);
+        for (char c : str.toCharArray()) {
+            if (c != stack.pop()) return false;
         }
-        for (char c : cleanedInput.toCharArray()) {
-            if (c != stack.pop()) {
-                return false;
-            }
+        return true;
+    }
+}
+
+class TwoPointerStrategy implements PalindromeStrategy {
+    public boolean check(String input) {
+        String str = input.toLowerCase();
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left++) != str.charAt(right--)) return false;
         }
         return true;
     }
