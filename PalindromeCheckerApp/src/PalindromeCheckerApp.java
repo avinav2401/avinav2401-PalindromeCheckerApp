a70acc9b449f1ab863096df7c27bcd9efe238262
avinav2401 <avinavpriyadrshi@gmail.com>
2026-03-02 10:43:23 +0530

    Your descriptive message for changes on UC7

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -9,25 +9,23 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a word : ");
-        String wrd=sc.nextLine(),temp = "";
-        Stack<Character> stack=new Stack<>();
-        Queue<Character> queue=new LinkedList<>();
-        for(char c:wrd.toCharArray())
-        {
-            stack.push(c);
-            queue.add(c);
-        }
-        boolean isPalindrome =true;
-        while(!queue.isEmpty())
-        {
-            if(queue.remove()!=stack.pop()) {
-                isPalindrome = false;
-                System.out.println("Not Palindrome");
-                System.exit(0);
-            }
-        }
-        if(isPalindrome)
-            System.out.println("Palindrome");
+       String wrd=sc.nextLine(),temp = "";
+       Deque<Character> deque=new ArrayDeque<>();
+       for(char c:wrd.toCharArray())
+       {
+           deque.addLast(c);
+       }
+       boolean isPalindrome =true;
+       while(deque.size()>1)
+       {
+           if(deque.removeLast()!=deque.removeFirst()) {
+               isPalindrome = false;
+               System.out.println("Not Palindrome");
+               System.exit(0);
+           }
+       }
+       if(isPalindrome)
+           System.out.println("Palindrome");
 
 
     }
