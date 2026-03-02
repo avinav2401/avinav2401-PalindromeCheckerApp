97ea6390c435ad1097e050951bfa994da25b7d1b
avinav2401 <avinavpriyadrshi@gmail.com>
2026-03-02 09:51:44 +0530

    Save current work

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,31 +1,12 @@
 /*
 This class will print the details of the App.
-* @author Anvinav priyadrshi
+* @author Avinav priyadrshi
+* @Version 1.0
 *
 * */
-import java.util.*;
+
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter a word : ");
-       String wrd=sc.nextLine(),temp = "";
-       Deque<Character> deque=new ArrayDeque<>();
-       for(char c:wrd.toCharArray())
-       {
-           deque.addLast(c);
-       }
-       boolean isPalindrome =true;
-       while(deque.size()>1)
-       {
-           if(deque.removeLast()!=deque.removeFirst()) {
-               isPalindrome = false;
-               System.out.println("Not Palindrome");
-               System.exit(0);
-           }
-       }
-       if(isPalindrome)
-           System.out.println("Palindrome");
-
-
+        System.out.println("Welcome to the Palindrome Checker Management System\nVersion : 1.0\nSystem initialized succesfully");
     }
 }
